c80d447a7e0aff6fbcf50c2c937c134ca6b603e2
程传航 <chengchuanhang@bonc.com.cn>
2017-10-24 17:19:07 +0800

    删除无用文件夹

--- .history/src/views/HomeScreen_20171020153300.js
+++
@@ -1,9 +0,0 @@
-class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Welcome'
-  };
-  render() {
-    return <Text>Hello, Navigation!</Text>;
-  }
-}
-export default HomeScreen
